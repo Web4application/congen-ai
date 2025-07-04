2a3b995207cb2d79776b60e34ec3a019ddf5c2b5
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-05 01:28:10 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,3 +3,6 @@ mkdocs new .
 mv docs/* into mkdocs-project/docs/
 mkdocs serve       # local
 mkdocs gh-deploy   # pushes to GitHub Pages
+
+docker compose up --build
+# → open http://localhost:8501
